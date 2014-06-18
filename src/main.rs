4d1fe0e9833c799dcbe5e27bf9c5035b4619756f
zzmp <zmp@umich.edu>
2014-06-17 21:15:38 -0700

    (doc) Added documentation.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 #![license = "MIT"]
-//#![deny(missing_doc)]
+#![deny(missing_doc)]
 #![deny(unused_result, unused_result, unnecessary_qualification,
         non_camel_case_types, unused_variable, unnecessary_typecast)]
+
+//! A seed project for the [Iron](https://github.com/iron/iron) web framework.
 
 extern crate iron;
 
@@ -10,6 +12,7 @@ use iron::{Iron, ServerT, Request, Response, Ingot, Alloy};
 use iron::ingot::{Status, Continue, Unwind};
 
 #[deriving(Clone)]
+// Create your own Ingot.
 struct HelloWorld;
 
 impl HelloWorld {
@@ -17,6 +20,7 @@ impl HelloWorld {
 }
 
 impl<Rq: Request, Rs: Response> Ingot<Rq, Rs> for HelloWorld {
+    // This defines the ingot's behavior when it first sees the Request.
     fn enter(&mut self, _req: &mut Rq, res: &mut Rs, _alloy: &mut Alloy) -> Status {
         match res.write(bytes!("Hello World!")) {
             Ok(_) => {
@@ -33,7 +37,7 @@ impl<Rq: Request, Rs: Response> Ingot<Rq, Rs> for HelloWorld {
 
 fn main() {
     let mut server: ServerT = Iron::new();
+    // Add the ingot to your server.
     server.smelt(HelloWorld::new());
     server.listen(Ipv4Addr(127, 0, 0, 1), 3000);
 }
-
